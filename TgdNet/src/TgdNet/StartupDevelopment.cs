7666c3ff2550cf87006de76ff5935cb373bfe983
LukaszSzulc <lukasz.schultz@gmail.com>
2015-11-07 21:45:38 +0100

    Separate Startups

---
+++ TgdNet/src/TgdNet/StartupDevelopment.cs
@@ -0,0 +1,31 @@
+﻿namespace TgdNet
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Hosting;
+    using Microsoft.Framework.Configuration;
+    using Microsoft.Framework.DependencyInjection;
+    using Microsoft.Dnx.Runtime;
+
+    public class StartupDevelopment
+    {
+        public StartupDevelopment(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            var configuration = new ConfigurationBuilder();
+            configuration.SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json", false)
+                .AddEnvironmentVariables();
+            this.Configuration = configuration.Build();
+        }
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
---
+++ TgdNet/src/TgdNet/StartupProduction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TgdNet
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Framework.Configuration;
+    using Microsoft.Framework.DependencyInjection;
+
+    public class StartupProduction
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async context => await context.Response.WriteAsync("Hello World"));
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+    }
+}
