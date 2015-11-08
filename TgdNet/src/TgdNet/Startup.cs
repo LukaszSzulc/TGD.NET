ffaa9db18e37e488e563ee9328620718aed0509c
LukaszSzulc <lukasz.schultz@gmail.com>
2015-11-08 21:18:57 +0100

    Initial Configuration

---
+++ TgdNet/src/TgdNet/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+
+namespace TgdNet
+{
+    using Microsoft.Framework.DependencyInjection;
+
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+}
